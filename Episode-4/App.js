1f2a147befbfd3e0a5ebf0e36084806bcd8d4f35
prashantpuri04 <prashantpuri04@gmail.com>
2023-11-07 23:33:19 +0530

    Episode-4-InitialCommit

---
+++ Episode-4/App.js
@@ -0,0 +1,55 @@
+// Food ordering app demo
+
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *    - RestaurantCard
+ *      - Img
+ *      - Name of Res, Star Rating, cuisine, delery tie
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Header = () => {
+    return(
+        <div className='header'>
+            <div className='logo-container'>
+                <img
+                className='logo'
+                src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+                />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+    <div className='app'>
+        <Header/>
+    </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
